1c133fab66a7c668a189a944c9606439a1f7734e
carons-cr <2738794789@qq.com>
2017-06-13 23:19:45 +0800

    回到最开始

--- main/main.js
+++ main/main.js
@@ -1,15 +1,8 @@
 const loadAllItems = require('./loadAllItems.js');
-module.exports = function main() {
-     let goodsList =new GoodsList(inputs);
-     return goodsList;	   
-     };
- function Goods(barcode,name,unit,price){
-	    	this.barcode=barcode;
-	    	this.name=name;
-	    	this.unit=unit;
-	    	this.price=price;
- };
+ 
 
+module.exports = function main(inputs) {
+     let goodsList =new GoodsList(inputs);
      var sgoodsList='***<没钱赚商店>购物清单***\n';
      sgoodsList+=goodsList.goodsInfo();
      sgoodsList+='----------------------\n';
@@ -31,11 +24,11 @@ module.exports = function main() {
       var allGoods=new Array();     
       allGoods=loadAllItems();
       var goodsArray =new Array();     
-      var typeGoods=new Array();      
+      var typeGoods=new Array();       
       typeGoods[0]=new Array();
       var s1='';
       for(var i=0;i<inputs.length;i++){
-  
+    	
             for(var j=0;j<allGoods.length;j++){
 	          if(allGoods[j].barcode==inputs[i]){
 			let goods = new Goods(allGoods[j].barcode,allGoods[j].name,allGoods[j].unit,allGoods[j].price);
@@ -43,12 +36,12 @@ module.exports = function main() {
 			continue;
 	          }
 	    }  
-
+      
            if((typeGoods[0].length==0)&&(typeGoods.length==1)){
 		   typeGoods[0][0]=goodsArray[0];
-            }else{                             
+            }else{                              
    	    	   var tag=0;
-		   for(var k=0;k<typeGoods.length;k++){            
+		   for(var k=0;k<typeGoods.length;k++){           
 		          if(goodsArray[i].barcode==(typeGoods[k][0].barcode)){
 				 var len1=typeGoods[k].length;
 				 typeGoods[k][len1]=goodsArray[i];
@@ -56,30 +49,30 @@ module.exports = function main() {
 				 continue;
 			  }
 	           }
-	           if(tag==0){                
+	           if(tag==0){                 
 			  var h=typeGoods.length;
 			  typeGoods[h]=new Array();
 			  typeGoods[h][0]=goodsArray[i];
 		   } 
-            } 
+             } 
        }
-      for(var i=0;i<typeGoods.length;i++){            
-    	     var count=0;
+       for(var i=0;i<typeGoods.length;i++){            
+   	     var count=0;
     	     var cost=0;
-    	     for(var j=0;j<typeGoods[i].length;j++){
+   	     for(var j=0;j<typeGoods[i].length;j++){
     	    	   count++;
     	     cost+=typeGoods[i][j].price;
     	     }
-   	     s1+='名称：'+typeGoods[i][0].name+'数量：'+count+typeGoods[i][0].unit
-    	     +单价：'+typeGoods[i][0].price.toFixed(2)+'(元)，小计：'+cost.toFixed(2)+'(元)\n';
+    	     s1=s1+'名称：'+typeGoods[i][0].name+'，数量：'+count+typeGoods[i][0].unit
+    	     +'，单价：'+typeGoods[i][0].price.toFixed(2)+'(元)，小计：'+cost.toFixed(2)+'(元)\n';
     	     this.sum=this.sum+cost;
-      } 
+       } 
     	    
        return s1;
     }; 
-   	
-    GoodsList.prototype.goodsTotal=function(){
-          var s2='总计：'+this.sum.toFixed(2)+'(元)\n';
+    	
+   GoodsList.prototype.goodsTotal=function(){
+         var s2='总计：'+this.sum.toFixed(2)+'(元)\n';
           return s2;
-    };       
+   };       
  };
